24e28809c06a26c3cd3501a1f1b13d98bb7cc36f
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-10 19:42:00 +0530

    Create B 203 : MULTIPLE'S OF 4.c

---
+++ B 203 : MULTIPLE'S OF 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int n;
+    scanf("%d",&n);
+    if(n%4==0)
+        printf("yes");
+    else printf("no");
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
