fa187ceccd2305b305960c8284a34faeca9c861c
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-10 19:44:52 +0530

    Create C 102 : Printing an Array in Reverse.c

---
+++ C 102 : Printing an Array in Reverse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i, arr[5];
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+              for(i=4;i>=0;i--)
+              {
+                  printf("%d ",arr[i]);
+              }
+              return 0;
+	//write the code here to print array in reverse
+
+
+	return 0;
+}
